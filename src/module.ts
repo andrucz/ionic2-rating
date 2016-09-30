9e07c48e98bd6ca229f66276cf4be1ec33998e1e
Acácio Andruczewicz <acacio.antonio@senior.com.br>
2016-09-30 11:36:34 -0300

    ionic 2 rc0 with angular 2 rc5.

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
 import { Ionic2Rating } from './ionic2-rating';
 
 const IONIC2_RATING_DIRECTIVES = [Ionic2Rating];
 
 @NgModule({
+  imports: [IonicModule],
   declarations: IONIC2_RATING_DIRECTIVES,
   exports: IONIC2_RATING_DIRECTIVES
 })
